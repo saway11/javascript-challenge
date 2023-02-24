2b24b2ad23d5ce97c83b0ffe8a906493aaf9ca61
unknown <rgarcia20.m@gmail.com>
2023-02-23 18:26:16 -0800

    init repo

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # javascript-challenge
+
+## Description
+
+The purpose of this challenge was to generate a password with 8-128 characters; while including lowercase, uppercase, special characters, and numbers. The main focus was in JavaScript as a code was to be created in order to generate random passwords.
+
+## Instalation
+
+The steps that were required to install my project included assigning a code, getting references to the #generate element, write a password to the #password input, and add event listener to generate the button. Withing each step I included functions that would push the letters and characters to generate on the password.
+
+## Usage
+IMG_1532.jpg
+
+## Credits
+Tutor: Mazin Abed
+TA: Maira
+W3 School
+UC Berkeley Coding bootcamp
+
+## License 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documention files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or subtantial portion of the Software.
+
+The Software is provided "as is", without warranty of any kind express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfrigement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with software or the use or other dealing in the software.
---
+++ script.js
@@ -0,0 +1,61 @@
+var characterLength = 8-128;
+var choiceArr = [];
+
+var specialCharArr =['~','!','@','#','$','%','^','&','*','(',')','_','-','=','+','<'];
+var lowerCaseArr =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr =['0','1','2','3','4','5','6','7','8','9'];
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var correctPrompts = getPrompts();
+  var passwordText = document.querySelector("#password");
+
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+}
+
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
+}
+
+function getPrompts() {
+  choiceArr = [];
+
+  characterLength = parseInt(prompt("How many characters would you like your password to be? (8-128)"));
+
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character length has to be a number between 8 and 128.");
+    return false;
+  }
+
+ if (confirm("Would you lowercase letters in your password?")) {
+  choiceArr = choiceArr.concat(lowerCaseArr);
+ }
+ if (confirm("Would you uppercase letters in your password?")) {
+  choiceArr = choiceArr.concat(upperCaseArr);
+ }
+ if (confirm("Would you special characters in your password?")) {
+  choiceArr = choiceArr.concat(specialCharArr);
+ }
+ if (confirm("Would you numbers in your password?")) {
+  choiceArr = choiceArr.concat(numberArr);
+ }
+ return true;
+}
